9306c288fbb44b89be428bc2e3ac54a2efba2837
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-09-02 18:00:01 -0400

    update for Base.stack

--- README.md
+++ README.md
@@ -1,38 +1,23 @@
 # LazyStack.jl
 
-[![Travis CI](https://travis-ci.org/mcabbott/LazyStack.jl.svg?branch=master)](https://travis-ci.org/mcabbott/LazyStack.jl)
 [![Github CI](https://github.com/mcabbott/LazyStack.jl/workflows/CI/badge.svg)](https://github.com/mcabbott/LazyStack.jl/actions?query=workflow%3ACI+branch%3Amaster)
 
-This package exports one function, `stack`, for turning a list of arrays 
+This package exports one function, `lazystack`, for turning a list of arrays 
 into one `AbstractArray`. Given several arrays with the same `eltype`, 
 or an array of such arrays, it returns a lazy `Stacked{T,N}` view of these:
 
 ```julia
-stack([zeros(2,2), ones(2,2)])  # isa Stacked{Float64, 3, <:Vector{<:Matrix}}
-stack([1,2,3], 4:6)             # isa Stacked{Int, 2, <:Tuple{<:Vector, <:UnitRange}}
+lazystack([zeros(2,2), ones(2,2)])  # isa Stacked{Float64, 3, <:Vector{<:Matrix}}
+lazystack([1,2,3], 4:6)             # isa Stacked{Int, 2, <:Tuple{<:Vector, <:UnitRange}}
 ```
 
-Given a generator, it instead iterates through the elements and writes into a new array.
-Given a function and then some arrays, it behaves like `map(f, A, B)` but immediately writes
-into a new array:
+Before v0.1 this function used to be called `stack`, but that name is now exported by Base (from Julia 1.9).
+However, `Base.stack` always returns a new dense array, not a lazy container.
 
-```julia
-stack([i,2i] for i in 1:5)            # isa Matrix{Int}     # size(ans) == (2, 5)
-stack(*, eachcol(ones(2,4)), 1:4)     # == Matrix(stack(map(*, eachcol(...), 1:4)))
-```
+Generators such as `lazystack([i,2i] for i in 1:5)` and arrays of mixed eltype like `lazystack([1,2], [3.0, 4.0], [5im, 6im])` used to be be handled here, and are now simply passed through to `Base.stack`.
 
-The same `stack_iter` method is also used for any list of arrays of heterogeneous element type,
-and for arrays of tuples. Notice that like `map(identity, Any[1, 1.0, 5im])`, this promotes using 
-`promote_typejoin`, to `Number` here, rather than to `Complex{Float64}`:
-
-```julia
-stack([1,2], [3.0, 4.0], [5im, 6im])  # isa Matrix{Number}  # size(ans) == (2, 3)
-stack([(i,2.0,3//j) for i=1:4, j=1:5])# isa Array{Real, 3}  # size(ans) == (3, 4, 5)
-```
-
-The slices must all have the same `size`, but they (and the container) 
-can have any number of dimensions. `stack` always places the slice dimensions first.
-There are no options.
+When the individual slices aren't backed by an Array, as for instance with `CuArray`s on a GPU, then again `Base.stack` is called.
+This should make one big `CuArray`, since scalar indexing of individual slices won't work well.
 
 ### Ragged stack
 
@@ -40,22 +25,19 @@ There is also a version which does not demand that slices have equal `size` (or 
 which always returns a new `Array`. You can control the position of slices `using OffsetArrays`:
 
 ```julia
-rstack([1:n for n in 1:10])           # upper triangular Matrix{Int}
-rstack(OffsetArray(fill(n,4), rand(-2:2)) for n in 1:10; fill=NaN)
+raggedstack([1:n for n in 1:10])           # upper triangular Matrix{Int}
+raggedstack(OffsetArray(fill(n,4), rand(-2:2)) for n in 1:10; fill=NaN)
 ```
 
 ### Other packages
 
-This one plays well with [OffsetArrays.jl](https://github.com/JuliaArrays/OffsetArrays.jl),
-[NamedDims.jl](https://github.com/invenia/NamedDims.jl), and 
-[Zygote.jl](https://github.com/FluxML/Zygote.jl).
+This one plays well with [OffsetArrays.jl](https://github.com/JuliaArrays/OffsetArrays.jl), and ChainRules-compatible AD such as [Zygote.jl](https://github.com/FluxML/Zygote.jl).
 
 Besides which, there are several other ways to achieve similar things:
 
 * For an array of arrays, you can also use [`JuliennedArrays.Align`](https://bramtayl.github.io/JuliennedArrays.jl/latest/#JuliennedArrays.Align). This requires (or enables) you to specify which dimensions of the output belong to the sub-arrays, instead of writing `PermutedDimsArray(stack(...), ...)`. 
 * There is also [`RecursiveArrayTools.VectorOfArray`](https://github.com/JuliaDiffEq/RecursiveArrayTools.jl#vectorofarray) which as its name hints only allows a one-dimensional container. Linear indexing retreives a slice, not an element, which is sometimes surprising.
 * For a tuple of arrays, [`LazyArrays.Hcat`](https://github.com/JuliaArrays/LazyArrays.jl#concatenation) is at present faster to index than `stack`, but doesn't allow arbitrary dimensions.
-* For a generator of arrays, the built-in `reduce(hcat,...)` may work, but it slow compared to `stack`: see [test/speed.jl](test/speed.jl) for some examples.
 
 And a few more:
 
@@ -77,4 +59,4 @@ Eager:
 
 * After writing this I learned of [JuliaLang#31644](https://github.com/JuliaLang/julia/pull/31644) which extends `reduce(hcat,...)` to work on generators. 
 
-* Later, [JuliaLang#31644](https://github.com/JuliaLang/julia/pull/43334) proposes to add the eager `stack_iter` method of this package to Base.
+* Later, [JuliaLang#43334](https://github.com/JuliaLang/julia/pull/43334) has added a better version of this package's `stack_iter` method to Base.
---
+++ src/flatten.jl
@@ -0,0 +1,300 @@
+export flatten
+using Base: IteratorSize, HasShape, HasLength, @default_eltype, haslength
+
+"""
+    flatten([T], iter)
+
+Given an iterator of iterators, returns a `Vector{T}` containing all of their elements.
+Should be equal to `collect(Iterators.flatten(iter))`, but faster.
+
+Acting on a vector of vectors, this is equal to `vcat(iter...)` or `reduce(vcat, iter)`,
+and to `vec(stack(iter))` when this is allowed, i.e. when `allequal(length.(iter))`.
+
+# Examples
+```
+julia> v = flatten((1:2, 8:9))
+4-element Vector{Int64}:
+ 1
+ 2
+ 8
+ 9
+
+julia> v == vcat(1:2, 8:9) == reduce(vcat, [1:2, 8:9]) == vec(stack((1:2, 8:9)))
+true
+
+julia> flatten(Complex{Float32}, [1:1, 9:10])  # specify eltype
+3-element Vector{ComplexF32}:
+  1.0f0 + 0.0f0im
+  9.0f0 + 0.0f0im
+ 10.0f0 + 0.0f0im
+
+julia> flatten(([1 2], [5.0], (x = false,)))  # ignores shape, promotes eltype
+4-element Vector{Float64}:
+ 1.0
+ 2.0
+ 5.0
+ 0.0
+
+julia> collect(Iterators.flatten(([1 2], [5.0], (x = false,))))  # wider eltype
+4-element Vector{Real}:
+     1
+     2
+     5.0
+ false
+
+julia> flatten(42)  # as numbers are iterable
+1-element Vector{Int64}:
+ 42
+
+julia> flatten(Vector{Int}[]), flatten(())  # empty case, with and without known eltype
+(Int64[], Union{}[])
+```
+"""
+function flatten end
+
+"""
+    flatten([T], f, args...)
+
+Equivalent to `flatten(map(f, args...))`, but without allocating the result of `map`.
+
+```
+julia> flatten(x -> (x, x/2), 5:6)
+4-element Vector{Real}:
+ 5
+ 2.5
+ 6
+ 3.0
+```
+"""
+flatten(f, iter) = flatten(f(x) for x in iter)
+flatten(f, xs, yzs...) = flatten(f(xy...) for xy in zip(xs, yzs...))
+
+flatten(::Type{T}, iter) where {T} = _typed_flatten(T, IteratorSize(@default_eltype iter), iter)
+flatten(::Type{T}, f, iter) where {T} = flatten(T, f(x) for x in iter)
+flatten(::Type{T},f, xs, yzs...) where {T} = flatten(T, f(xy...) for xy in zip(xs, yzs...))
+
+function flatten(iter)
+  S = @default_eltype iter
+  T = S != Union{} ? eltype(S) : Any  # Union{} occurs for e.g. flatten(1,2), postpone the error
+  if isconcretetype(T)
+      _typed_flatten(T, IteratorSize(S), iter)
+  else
+      _untyped_flatten(iter)
+  end
+end
+
+function _typed_flatten(::Type{T}, ::Union{HasShape, HasLength}, A::Union{AbstractArray, Tuple}) where {T}
+    len = sum(length, A; init=0)
+    B = Vector{T}(undef, len)
+    off = 1
+    for x in A
+        copyto!(B, off, x)
+        off += length(x)
+    end
+    B
+end
+
+# _typed_flatten(::Type{T}, ::Union{HasShape, HasLength}, A) where {T} = _flatten(collect(A))
+
+function _typed_flatten(::Type{T}, ::IteratorSize, A) where {T}
+    xit = iterate(A)
+    nothing === xit && return T[]
+    x1, _ = xit
+    B = Vector{T}(undef, 0)
+    if haslength(x1) && haslength(A)
+        # sizehint!(B, _flatten_alloc_length(T, x1, A))
+        guess = length(x1) * length(A)
+        alloc = min(guess, (2^30) ÷ max(1, sizeof(T)))  # don't allocate > 1GB
+        sizehint!(B, alloc)
+    end
+    while xit !== nothing
+        x, state = xit
+        append!(B, x)
+        xit = iterate(A, state)
+    end
+    B
+end
+
+# function _flatten_alloc_length(::Type{T}, x1, A) where {T}
+#     guess = length(x1) * length(A)
+#     min(guess, (2^30) ÷ max(1, sizeof(T)))  # don't allocate > 1GB
+# end
+
+function _untyped_flatten(A)
+    xit = iterate(A)
+    nothing === xit && return Union{}[]
+    x1, state = xit
+    B = convert(Vector, vec(collect(x1)))  # can you do this better?
+    # if haslength(x1) && haslength(A)  # doesn't seem to help
+    #     sizehint!(B, _flatten_alloc_length(eltype(B), B, A))
+    # end
+    _untyped_flatten!(B, A, state)
+end
+
+#= For the empty case:
+
+julia> collect(Iterators.flatten(()))
+Union{}[]
+
+=#
+
+function _untyped_flatten!(B, A, state)
+    xit = iterate(A, state)
+    while xit !== nothing
+        x, state = xit
+        if eltype(x) <: eltype(B)
+            append!(B, x)
+        else
+            C = vcat(B, vec(collect(x)))  # can you do this better?
+            return _untyped_flatten!(C, A, state)
+        end
+        xit = iterate(A, state)
+    end
+    return B
+end
+
+
+
+#=
+
+let
+    tups = [Tuple(rand(10)) for _ in 1:100]
+    println("vector of tuples")
+    @btime stack($tups)
+    @btime flatten($tups)
+    @btime collect(Iterators.flatten($tups))
+
+    println("generator of tuples")
+    @btime stack(t for t in $tups)
+    @btime flatten(t for t in $tups)
+    @btime collect(Iterators.flatten(t for t in $tups))
+
+    println("  ... extra to collet")
+    @btime collect(t for t in $tups)
+
+    println("vectors")
+    vecs = [rand(100) for _ in 1:100]
+    @btime stack($vecs)
+    @btime flatten($vecs)
+    @btime collect(Iterators.flatten($vecs))
+
+    println("generator of vectors")
+    @btime stack(v for v in $vecs)
+    @btime flatten(v for v in $vecs)
+    @btime collect(Iterators.flatten(v for v in $vecs))
+
+    println("  ... extra to collet")
+    @btime collect(t for t in $vecs)
+end;
+
+vector of tuples
+  min 952.636 ns, mean 2.109 μs (1 allocation, 7.94 KiB)
+  min 845.333 ns, mean 2.071 μs (1 allocation, 7.94 KiB)
+  min 11.708 μs, mean 12.503 μs (1 allocation, 7.94 KiB)
+generator of tuples
+  min 945.800 ns, mean 2.055 μs (1 allocation, 7.94 KiB)
+  min 1.608 μs, mean 2.613 μs (2 allocations, 7.94 KiB)   <--- could be better?
+  min 16.292 μs, mean 18.288 μs (7 allocations, 21.92 KiB)
+  ... extra to collet
+  min 482.051 ns, mean 1.713 μs (1 allocation, 7.94 KiB)
+vectors
+  min 2.643 μs, mean 7.749 μs (2 allocations, 78.17 KiB)
+  min 2.704 μs, mean 7.681 μs (2 allocations, 78.17 KiB)
+  min 70.375 μs, mean 77.521 μs (9 allocations, 326.55 KiB)
+generator of vectors
+  min 2.588 μs, mean 7.277 μs (2 allocations, 78.17 KiB)
+  min 4.738 μs, mean 9.588 μs (2 allocations, 78.25 KiB)   <--- could be better
+  min 68.625 μs, mean 76.385 μs (10 allocations, 326.61 KiB)
+  ... extra to collet
+  min 192.396 ns, mean 217.751 ns (1 allocation, 896 bytes)  <--- worth it!
+
+
+let
+    tups = [Tuple(rand(10)) for _ in 1:100]
+    println("unknown number of tuples")
+    @btime stack(t for t in $tups if true)
+    @btime flatten(t for t in $tups if true)
+    @btime collect(Iterators.flatten(t for t in $tups if true))
+
+    println("  ... extra to collet")
+    @btime collect(t for t in $tups if true)
+
+    vecs = [rand(100) for _ in 1:100]
+    println("unknown number of vectors")
+    @btime stack(v for v in $vecs if true)
+    @btime flatten(v for v in $vecs if true)
+    @btime collect(Iterators.flatten(v for v in $vecs if true))
+
+    println("  ... extra to collet")
+    @btime collect(v for v in $vecs if true)
+end;
+
+unknown number of tuples
+  min 2.454 μs, mean 5.807 μs (6 allocations, 25.72 KiB)
+  min 2.042 μs, mean 4.953 μs (6 allocations, 21.91 KiB)
+  min 16.292 μs, mean 18.510 μs (7 allocations, 21.92 KiB)
+  ... extra to collet
+  min 1.450 μs, mean 3.878 μs (5 allocations, 17.78 KiB)
+unknown number of vectors
+  min 3.625 μs, mean 9.854 μs (7 allocations, 80.12 KiB)
+  min 7.917 μs, mean 16.509 μs (7 allocations, 198.11 KiB)   <--- could be better?
+  min 71.875 μs, mean 79.867 μs (10 allocations, 326.61 KiB)
+  ... extra to collet
+  min 1.042 μs, mean 1.158 μs (5 allocations, 1.95 KiB)  <--- worth it!
+
+let
+    nums = rand(128, 100)
+    println("vector")
+    @btime stack($nums)
+    @btime flatten($nums)
+    @btime copy(vec($nums))
+end;
+
+vector
+  min 8.361 μs, mean 20.294 μs (2 allocations, 100.05 KiB)
+  min 9.333 μs, mean 19.566 μs (2 allocations, 100.05 KiB)
+  min 1.995 μs, mean 10.940 μs (4 allocations, 100.12 KiB)
+
+let
+    tups = [Tuple(rand(rand(1:20))) for _ in 1:100]
+    println("tuples of varying length")
+    @btime flatten($tups)
+    @btime collect(Iterators.flatten($tups))
+
+    vecs = [rand(1:200) for _ in 1:100]
+    println("vectors of varying length")
+    @btime flatten($vecs)
+    @btime collect(Iterators.flatten($vecs))
+end;
+
+tuples of varying length
+  min 14.167 μs, mean 15.792 μs (52 allocations, 9.11 KiB)
+  min 8.833 μs, mean 11.346 μs (6 allocations, 21.86 KiB)
+vectors of varying length
+  min 105.169 ns, mean 125.837 ns (1 allocation, 896 bytes)
+  min 106.701 ns, mean 127.880 ns (1 allocation, 896 bytes)
+
+let
+    tups = vcat([Tuple(rand(1:10, 10)) for _ in 1:50], [Tuple(rand(10)) for _ in 1:50])
+    println("tuples of varying eltype")
+    @btime stack($tups)
+    @btime flatten($tups)
+    @btime collect(Iterators.flatten($tups))
+
+    println("vectors of varying eltype")
+    vecs = vcat([rand(1:10, 100) for _ in 1:50], [rand(100) for _ in 1:50])
+    @btime stack($vecs)
+    @btime flatten($vecs)
+    @btime collect(Iterators.flatten($vecs))
+end;
+
+tuples of varying eltype
+  min 23.917 μs, mean 26.051 μs (49 allocations, 8.69 KiB)
+  min 10.417 μs, mean 13.462 μs (7 allocations, 20.41 KiB)
+  min 28.041 μs, mean 29.826 μs (501 allocations, 15.75 KiB)
+vectors of varying eltype
+  min 2.646 μs, mean 10.090 μs (2 allocations, 78.17 KiB)
+  min 2.713 μs, mean 9.107 μs (2 allocations, 78.17 KiB)
+  min 68.750 μs, mean 77.771 μs (9 allocations, 326.55 KiB)
+
+=#
